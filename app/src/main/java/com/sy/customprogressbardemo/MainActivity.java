2fed8356b2cba370b6a71c2f9b6de7f715de44fc
SY <18795883226@163.com>
2017-10-19 22:25:18 +0800

    commit test  1

--- app/src/main/java/com/sy/customprogressbardemo/MainActivity.java
+++ app/src/main/java/com/sy/customprogressbardemo/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 mCpb_one.sub();
                 //111111111111
+               // 123231
             }
         });
     }
